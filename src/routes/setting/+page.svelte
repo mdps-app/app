4a951c270cf427ab764742bf6d9ed6d9c84e40c3
shiki <kishintorii@gmail.com>
2024-03-27 23:37:02 +0900

    📥 add
    メールでのアカウント作成・パスワード変更機能の追加だよん

--- src/routes/setting/+page.svelte
+++ src/routes/setting/+page.svelte
@@ -1,11 +1,75 @@
-<script>
-	import { tTop , check } from '$lib/pageMove';
-    import { onMount } from 'svelte';
+<script lang="ts">
+	import { tTop, check } from '$lib/pageMove';
+	import { onMount } from 'svelte';
 	onMount(() => {
 		check.set(false);
 	});
 
 	tTop.set(490);
+
+	import Signin from '../signin.svelte';
+	import { goto } from '$app/navigation';
+	import { onAuthStateChanged } from 'firebase/auth';
+	import { auth, db } from '$lib/firebase';
+	import { signOut } from 'firebase/auth';
+
+	let userId: string | null;
+	onAuthStateChanged(auth, (user) => {
+		userId = user ? user.uid : null;
+	});
+	import { setupAuthListener } from '$lib/logg.js';
+	import { query, collection, where, getDocs } from 'firebase/firestore';
+	let user = null;
+	onMount(() => {
+		const unsubscribe = setupAuthListener(async (currentUser) => {
+			user = currentUser;
+			if (user) {
+				const q = query(collection(db, 'approvedUsers'), where('uid', '==', user.uid));
+				const querySnapshot = await getDocs(q);
+				if (!querySnapshot.empty) {
+				} else {
+					signOut(auth);
+					alert('承認されたアカウントでないため、ログアウトしました');
+				}
+			} else {
+			}
+		});
+		return unsubscribe;
+	});
+
+	import { updatePassword } from 'firebase/auth';
+
+	let newPass: string;
+	async function updatePass(newPassword: string) {
+		const user = auth.currentUser;
+		if (user) {
+			try {
+				await updatePassword(user, newPassword);
+				console.log('Password updated successfully');
+				signOut(auth);
+			} catch (error) {
+				console.error('Error updating password: ', error);
+			}
+		} else {
+			console.error('No user is currently logged in');
+		}
+	}
 </script>
 
 <h1>setting</h1>
+{#if userId}
+	<div class="fixed right-2 top-2">
+		<button
+			class="underline"
+			on:click={() => {
+				goto(window.location.pathname, { replaceState: true });
+				signOut(auth);
+			}}>Logout</button
+		>
+		<p>user:{userId}</p>
+	</div>
+	<input type="text" placeholder="new password" bind:value={newPass}>
+	<button on:click={() => updatePass(newPass)}>パスワード変更</button>
+{:else if userId === null}
+	<Signin />
+{/if}
